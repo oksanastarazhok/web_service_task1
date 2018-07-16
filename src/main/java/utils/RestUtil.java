c328885b4aef88d240bb872c0e0716cc75dc15e0
oksanastarazhok <Oksana_Starazhok@epam.com>
2018-07-16 14:03:18 +0300

    added 2 verifications

--- src/main/java/utils/RestUtil.java
+++ src/main/java/utils/RestUtil.java
@@ -12,35 +12,22 @@ import java.io.IOException;
 public class RestUtil {
 
       /**
-     * This method creates request
+     * This method creates request and returns response from a server in String format
      * @param baseURL URL that is need to be verified
-     */
-    public static HttpGet createRequest(String baseURL) {
-        HttpGet httpGet = new HttpGet(baseURL);
-        return httpGet;
-    }
-
-
-    /**
-     * This method returns response from a server in String format
-     * @param httpGet request to a server
      * @return response from a server
      * @throws IOException
      */
-    public static String getResponse(HttpGet httpGet) throws IOException {
+    public static CloseableHttpResponse getResponse(String baseURL) throws IOException {
+        HttpGet httpGet = new HttpGet(baseURL);
         CloseableHttpClient httpclient = HttpClients.createDefault();
         CloseableHttpResponse response = httpclient.execute(httpGet);
-        HttpEntity entity = response.getEntity();
-        String responseString = EntityUtils.toString(entity, "UTF-8");
-        return responseString;
+        return response;
+
     }
 
-    /**
-     * Reset Base URI (after test)
-     * @param baseURL
-     */
-    public static void resetBaseURL(String baseURL) {
-        baseURL = null;
-    }
-
+public static String getString(CloseableHttpResponse response)throws IOException {
+    HttpEntity entity = response.getEntity();
+    String responseString = EntityUtils.toString(entity, "UTF-8");
+    return responseString;
+}
 }
--- src/test/java/apiTests/APITest.java
+++ src/test/java/apiTests/APITest.java
@@ -1,15 +1,30 @@
 package apiTests;
 
-import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.RestUtil;
 
+import java.io.IOException;
+
+
 public class APITest {
 
-    @Test(description = "Positive test for countryCode= USA and valid stateCode=ID")
-    public void  positiveTest(){
-       HttpGet httpget = RestUtil.createRequest("http://services.groupkt.com/state/get/USA/ID");
 
 
+    @Test(description = "Verify response is 200 for countryCode = USA and valid stateCode=ID", priority = 0)
+
+    public void  positiveHTTPResponseStatusTest() throws IOException {
+        CloseableHttpResponse serverResponse = RestUtil.getResponse("http://services.groupkt.com/state/get/USA/ID");
+        Assert.assertEquals(serverResponse.getStatusLine().getStatusCode(), 200);
     }
+
+    @Test(description = "Verify capital is Boise for countryCode = USA and valid stateCode=ID", priority = 2)
+    public void positiveCapitalTest(CloseableHttpResponse serverResponse) throws IOException {
+      String responseString =  RestUtil.getString(serverResponse);
+        boolean checkCapital = responseString.contains("Boise");
+        Assert.assertTrue(checkCapital);
+    }
+
+
 }
