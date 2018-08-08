8907c2a5ef0a49c5f492609b0055a242524f92e0
oksanastarazhok <Oksana_Starazhok@epam.com>
2018-08-08 10:58:31 +0300

    removed priorities

--- src/test/java/apiTests/APITest.java
+++ src/test/java/apiTests/APITest.java
@@ -21,14 +21,14 @@ public class APITest {
 
     }
 
-    @Test(description = "Verify response is 200 for countryCode = USA and valid stateCode=ID", priority = 1)
+    @Test(description = "Verify response is 200 for countryCode = USA and valid stateCode=ID")
     public void positiveHTTPResponseStatusTest() throws IOException {
         CloseableHttpResponse serverResponse = RestUtils.getResponse(URLExpectedPositiveResponse);
         Assert.assertEquals(serverResponse.getStatusLine().getStatusCode(), 200);
 
     }
 
-    @Test(description = "Verify capital is Boise for countryCode = USA and valid stateCode=ID", priority = 200)
+    @Test(description = "Verify capital is Boise for countryCode = USA and valid stateCode=ID")
     public void positiveCapitalTest() throws IOException {
         CloseableHttpResponse serverResponse = RestUtils.getResponse(URLExpectedPositiveResponse);
         String responseString = RestUtils.getResponseAsString(serverResponse);
@@ -44,7 +44,7 @@ public class APITest {
 
     }
 
-    @Test(description = "Verify body contains correct validation error", priority = 200)
+    @Test(description = "Verify body contains correct validation error")
     public void correctValidationErrorTest() throws IOException {
         CloseableHttpResponse serverResponse = RestUtils.getResponse(URLExpectedNegativeResponse);
         String responseString = RestUtils.getResponseAsString(serverResponse);
